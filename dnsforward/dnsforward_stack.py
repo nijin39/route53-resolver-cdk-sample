9bf4903d4bb31c863a54db1fa5e53623b72df7b2
Ubuntu <ubuntu@ip-172-31-38-197.ap-northeast-2.compute.internal>
2020-02-11 02:07:36 +0000

    Add VPC Configuration

--- dnsforward/dnsforward_stack.py
+++ dnsforward/dnsforward_stack.py
@@ -1,8 +1,5 @@
 from aws_cdk import (
-    aws_iam as iam,
-    aws_sqs as sqs,
-    aws_sns as sns,
-    aws_sns_subscriptions as subs,
+    aws_ec2 as ec2,
     core
 )
 
@@ -10,14 +7,33 @@ class DnsforwardStack(core.Stack):
 
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
-
-        queue = sqs.Queue(
-            self, "DnsforwardQueue",
-            visibility_timeout=core.Duration.seconds(300),
+        
+        vpc = ec2.Vpc(self, "VPC",
+            cidr="10.0.0.0/21",
+            max_azs=3,
+            subnet_configuration=[ec2.SubnetConfiguration(
+                cidr_mask=24,
+                name="Application",
+                subnet_type=ec2.SubnetType.PUBLIC
+                ),
+                ec2.SubnetConfiguration(
+                cidr_mask=24,
+                name="Application2",
+                subnet_type=ec2.SubnetType.PUBLIC
+                )
+            ]
         )
 
-        topic = sns.Topic(
-            self, "DnsforwardTopic"
+        security_group = ec2.SecurityGroup(
+            self,
+            id='test-security-group',
+            vpc=vpc,
+            security_group_name='test-security-group'
         )
-
-        topic.add_subscription(subs.SqsSubscription(queue))
+        
+        security_group = ec2.SecurityGroup(
+            self,
+            id='test-security-group',
+            vpc=vpc,
+            security_group_name='test-security-group'
+        )
