6bf064909dffa46d25c4af0636ffbd0fb49e03ee
Ubuntu <ubuntu@ip-172-31-38-197.ap-northeast-2.compute.internal>
2020-02-11 02:12:12 +0000

    Modify VPC SG

--- dnsforward/dnsforward_stack.py
+++ dnsforward/dnsforward_stack.py
@@ -8,8 +8,10 @@ class DnsforwardStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
         
+        cidr = "10.0.0.0/21"
+        
         vpc = ec2.Vpc(self, "VPC",
-            cidr="10.0.0.0/21",
+            cidr=cidr,
             max_azs=3,
             subnet_configuration=[ec2.SubnetConfiguration(
                 cidr_mask=24,
@@ -31,9 +33,7 @@ class DnsforwardStack(core.Stack):
             security_group_name='test-security-group'
         )
         
-        security_group = ec2.SecurityGroup(
-            self,
-            id='test-security-group',
-            vpc=vpc,
-            security_group_name='test-security-group'
+        security_group.add_ingress_rule(
+            peer=ec2.Peer.ipv4(cidr),
+            connection=ec2.Port.tcp(22),
         )
